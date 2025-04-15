037998a0a87b5ec722630d8f328c680ce8f3917b
Nagabhushan <gowdagowda452@gmail.com>
2025-04-15 23:15:20 +0530

    added new file

---
+++ ZestyBeanz/Models/__init__.py
@@ -0,0 +1,3 @@
+from . import model_one
+from . import foodfrom 
+from . import car_rental
--- ZestyBeanz/__manifest__.py
+++ ZestyBeanz/__manifest__.py
@@ -1,30 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Odoo, Open Source Management Solution
+# Copyright (C) 2025 ZestyBeanz Technologies(<http://www.zbeanztech.com>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 
 {
-'name': 'Sample Module',
-'version': '18.0.0.0',
-'summary': 'This Module is for training purposes.',
-'description': """This Module is for training purposes.
-""",
-'category':'',
-'author': ' Nagabhushan',
-'website': 'www.zbeanztech.com',
-"license": "LGPL-3",
-'depends': ['sale','sale_management','account','contacts','product'],
-'data': [
-    
-    'Security/security.xml', # Always keep security fiels @ top
-    'Security/ir.model.access.csv', # Always keep security fiels @ top
-    'Views/model_one_view.xml',
-    'Views/food.xml',
-    'Views/menu.xml',
-    'Views/car_rental.xml',
-    'Data/sequence.xml',
-    'Views/model_one_lines.xml',
-    
-],
-'test': [],
-'demo': [],
-'installable': True,
-'auto_install': False,
-'application': False,
+    'name': 'ZestyBeanz',  
+    'version': '18.0.0.0',
+    'summary': 'This Module is for training purposes.',
+    'description': """This Module is for training purposes.""",
+    'category': '',
+    'author': 'Nagabhushan',
+    'website': 'www.zbeanztech.com',
+    "license": "LGPL-3",
+    'depends': ['sale','sale_management','account','contacts','product'],
+    'data': [
+        'Security/ir.model.access.csv',
+        'Security/security.xml',
+        'Data/sequence.xml',
+        'Views/car_rental.xml',
+        'Views/food.xml',
+        'Views/car_rental.xml',
+        'Views/model_one_view.xml',
+        'Views/model_one_lines.xml',
+        'Views/menu.xml',
+    ],
+    'test': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
 }
