3ba9e34178a232ce4d8ecbe9dc9d3ff5778ac7ac
Nagabhushan <gowdagowda452@gmail.com>
2025-04-24 00:10:31 +0530

    m2m commands

--- sample_module/models/model_one.py
+++ sample_module/models/model_one.py
@@ -9,14 +9,26 @@ class ModelOne(models.Model):
     age = fields.Integer(string="Age", default=18)
     email = fields.Char(string="Email", required=True, copy=False)
     joining_date = fields.Date(string="Joining Date", required=True)
-    date = fields.Date(string='Date')  # Ensure this field is defined
+    date = fields.Date(string='Date')
     gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], string="Gender", required=True, copy=False)
     active = fields.Boolean("Active", default=True)
     description = fields.Text("Description", default="Test Description")
     partner_ids = fields.Many2many('res.partner', string="Partner")
     sale_ids = fields.Many2many('sale.order', 'model_one_sale_rel', 'model_one_id', 'sale_id', string="Sale")
+    product_ids = fields.Many2many('product.template', 'model_one_prduct_rel', 'model_one_id', 'product_id',  string="Products")
     model_one_line_ids = fields.One2many('model.one.lines', 'model_one_id', string="Product")
     sale_id = fields.Many2one('sale.order', string="Sales")
+
+    def write_values(self):
+        products = self.env['product.template'].search([('list_price', '>', 200)], limit=1).id
+        order = self.env['sale.order'].search([('id', '=', 26)], limit=1).id
+        #self.write({'product_ids' : [[6, 0, products]]})     #replace values
+        #self.write({'product_ids' : [[5]]})      #unlnk all records
+        #self.write({'product_ids' : [[4, products]]})          #link to existing record
+        #self.write({'product_ids' : [[3, products]]})        #unlink a record (don't delete)
+        #self.write({'sale_ids' : [[2, order]]})    #unlinks and deletes the record 
+    
+        self.write({'product_ids' : [[0.0, {'name': 'Test Product', 'list price':200}]]}) 
 
     def helloworld(self):
         print("Hello World")
@@ -47,6 +59,4 @@ class ModelOnelines(models.Model):
     price = fields.Float(string="Standard Price")
     product_id = fields.Many2one('product.template', string="Product")
     model_one_id = fields.Many2one('model.one', string="Model One", domain="['|',('gender', '=', 'female'),('age','>',18)]")
-
-
 
