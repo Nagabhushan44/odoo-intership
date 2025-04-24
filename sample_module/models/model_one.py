b7138efee3c64a738dd605f43aa28484a8973b3e
Nagabhushan <gowdagowda452@gmail.com>
2025-04-24 17:36:21 +0530

    upto date

--- sample_module/models/model_one.py
+++ sample_module/models/model_one.py
@@ -10,30 +10,48 @@ class ModelOne(models.Model):
     email = fields.Char(string="Email", required=True, copy=False)
     joining_date = fields.Date(string="Joining Date", required=True)
     date = fields.Date(string='Date')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], string="Gender", required=True, copy=False)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('other', 'Other')], 
+                            string="Gender", required=True, copy=False)
     active = fields.Boolean("Active", default=True)
     description = fields.Text("Description", default="Test Description")
     partner_ids = fields.Many2many('res.partner', string="Partner")
     sale_ids = fields.Many2many('sale.order', 'model_one_sale_rel', 'model_one_id', 'sale_id', string="Sale")
-    product_ids = fields.Many2many('product.template', 'model_one_prduct_rel', 'model_one_id', 'product_id',  string="Products")
+    product_ids = fields.Many2many('product.template', 'model_one_prduct_rel', 'model_one_id', 'product_id', string="Products")
     model_one_line_ids = fields.One2many('model.one.lines', 'model_one_id', string="Product")
     sale_id = fields.Many2one('sale.order', string="Sales")
 
     def write_values(self):
+        """
+        Method to demonstrate various write operations on many2many and one2many fields
+        """
+        # Example operations for many2many fields
         products = self.env['product.template'].search([('list_price', '>', 200)], limit=1).id
         order = self.env['sale.order'].search([('id', '=', 26)], limit=1).id
-        #self.write({'product_ids' : [[6, 0, products]]})     #replace values
-        #self.write({'product_ids' : [[5]]})      #unlnk all records
-        #self.write({'product_ids' : [[4, products]]})          #link to existing record
-        #self.write({'product_ids' : [[3, products]]})        #unlink a record (don't delete)
-        #self.write({'sale_ids' : [[2, order]]})    #unlinks and deletes the record 
-    
-        self.write({'product_ids' : [[0.0, {'name': 'Test Product', 'list price':200}]]}) 
+        
+        # self.write({'product_ids': [[6, 0, products]]})     # replace values
+        # self.write({'product_ids': [[5]]})                  # unlink all records
+        # self.write({'product_ids': [[4, products]]})        # link to existing record
+        # self.write({'product_ids': [[3, products]]})        # unlink a record (don't delete)
+        # self.write({'sale_ids': [[2, order]]})             # unlinks and deletes the record 
+
+        # Operations for one2many fields
+        # self.write({'model_one_line_ids': [[0, 0, {'name': 'Test 1', 'product_id': products, 'price': 250}]]})    
+        # line = self.model_one_line_ids.filtered(lambda l: l.price == 300).id
+        # existing_line = self.env['model.one.lines'].search([('price', '=', 300)], limit=1).id
+        ex_line = self.env['model.one.lines'].search([('model_one_id', '=', False)], limit=1).id
+        # self.write({'model_one_line_ids': [[1, line, {'price': 350}]]}) 
+        # self.write({'model_one_line_ids': [[2, line]]})  
+        # self.write({'model_one_line_ids': [[3, line]]})  
+        # self.write({'model_one_line_ids': [[4, existing_line]]})  
+        # self.write({'model_one_line_ids': [[4, ex_line]]})  
+        self.write({'model_one_line_ids': [[6, 0, ex_line]]})     
 
     def helloworld(self):
+        """Simple test method"""
         print("Hello World")
 
     def show_wizard(self):
+        """Method to show a wizard"""
         return {
             'type': 'ir.actions.act_window',
             'name': 'My Sample Wizard',
@@ -46,10 +64,10 @@ class ModelOne(models.Model):
 
     @api.model
     def create(self, vals):
-        print('----------', self.env['ir.sequence'])
+        """Override create method to set sequence"""
         vals['seq'] = self.env['ir.sequence'].next_by_code('sequence.model.one')
-        res = super(ModelOne, self).create(vals)
-        return res
+        return super(ModelOne, self).create(vals)
+
 
 class ModelOnelines(models.Model):
     _name = "model.one.lines"
@@ -58,5 +76,5 @@ class ModelOnelines(models.Model):
     name = fields.Char(string="Name", help='A normal name field')
     price = fields.Float(string="Standard Price")
     product_id = fields.Many2one('product.template', string="Product")
-    model_one_id = fields.Many2one('model.one', string="Model One", domain="['|',('gender', '=', 'female'),('age','>',18)]")
-
+    model_one_id = fields.Many2one('model.one', string="Model One", 
+                                 domain="['|',('gender', '=', 'female'),('age','>',18)]")
