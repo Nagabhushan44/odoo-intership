53bfae9c425d13b1b4721b2282b7281ec61d2ef1
Nagabhushan <gowdagowda452@gmail.com>
2025-04-25 16:10:25 +0530

    api methods

--- sample_module/models/model_one.py
+++ sample_module/models/model_one.py
@@ -19,6 +19,10 @@ class ModelOne(models.Model):
     product_ids = fields.Many2many('product.template', 'model_one_prduct_rel', 'model_one_id', 'product_id', string="Products")
     model_one_line_ids = fields.One2many('model.one.lines', 'model_one_id', string="Product")
     sale_id = fields.Many2one('sale.order', string="Sales")
+    
+    partner_count = fields.Integer(string="Partner Count", compute="get_partner_count")
+    is_special = fields.Boolean('Is Special')
+
 
     def write_values(self):
         """
@@ -57,16 +61,39 @@ class ModelOne(models.Model):
             'name': 'My Sample Wizard',
             'res_model': 'sample.wizard',
             'view_mode': 'form',
-            'view_id': self.env.ref('ZestyBeanz.view_form_sample_wizard').id,
+            'view_id': self.env.ref('sample_module.view_form_sample_wizard').id,
             'target': 'new',
-            'context': {'default_name': 'Thunder'}
+            'context': {'default_name': 'Naga'}
         }
 
+    #@api method decorators
+	#1. @api.model : model level operations, don't need recordsets
     @api.model
     def create(self, vals):
         """Override create method to set sequence"""
         vals['seq'] = self.env['ir.sequence'].next_by_code('sequence.model.one')
-        return super(ModelOne, self).create(vals)
+        res=super(ModelOne, self).create(vals)
+        return res
+
+    #2. @api.depends : define dependencies between models and fields
+    @api.depends('partner_ids')
+    def get_partner_count(self):
+        for record in self:
+            if record.partner_ids:
+                record.partner_count = len(record.partner_ids)
+            else:
+                record.partner_count = 0
+    
+    #3. @api.onchange : execute your logic when there is a change in a field
+    @api.onchange('gender')
+    def onchange_gender(self):
+        for record in self:
+            if record.gender == 'other':
+                record.is_special = True
+            else:
+                record.is_special = False
+	
+
 
 
 class ModelOnelines(models.Model):
